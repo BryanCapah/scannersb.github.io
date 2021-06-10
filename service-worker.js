45785b02c78beca8d3f4d3ae52be3d138a5c982a
Brian Capah <hanjar.muhammad@onebyone.com.sg>
2021-06-10 15:15:06 +0700

    asd

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.acaa8fdd5adea5fe505c56685d59a072.js"
+  "./precache-manifest.c2d64808e23ce230230cfe2aee290064.js"
 );
 
 self.addEventListener('message', (event) => {
